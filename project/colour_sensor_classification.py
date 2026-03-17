a0a5e6b24e7aa0cc9f6c1438da2e41d3acc29f95
Janelle Tam <janelle.tam@mail.mcgill.ca>
2026-03-17 12:07:13 -0400

    add initial classification logic for colour sensor subsystem

--- project/colour_sensor_classification.py
+++ project/colour_sensor_classification.py
@@ -1,66 +1,61 @@
 from utils.brick import EV3ColorSensor, wait_ready_sensors
 from time import sleep
-import keyboard
+from config.settings import COLOUR_READINGS_MAP, COLOUR_LUMINOSITY_MAP
+import math
 
-COLOUR_SENSOR_DATA_FILE = "../data_analysis/colour_sensor_data.csv"
 COLOUR_SENSOR = EV3ColorSensor(3)
-COLOUR_MAP = {}
 
-# loop -> for each colour (red, green, blue, yellow, orange)
-# prompt user: hold the block in front of the sensor for 3 seconds
-# program should normalize the values, average them, and write to a csv file with the format: colour, r, g, b, intensity
+def normalize_reading(color_data):
+    r, g, b = color_data[:3]
+
+    # normalization
+    denominator = float(r) + float(g) + float(b)
+    if denominator != 0:
+        r_norm = float(r) / denominator
+        g_norm = float(g) / denominator
+        b_norm = float(b) / denominator
+    else:
+        r_norm = g_norm = b_norm = 0
+        
+    return r_norm, g_norm, b_norm, denominator   
+
+def classify_color(color_data):
+    best_match = ""
+    min_distance = float('inf')
+    
+    luminosity = color_data[3]
+    color_data = color_data[:3]
+    
+    # check if B or W (only look at luminosity)
+    for (min_luminosity, max_luminosity), colour in COLOUR_LUMINOSITY_MAP:
+        if min_luminosity <= luminosity <= max_luminosity:
+            return colour
+        
+    # check other colours (look at euclidean distance)
+    for (reference_point, color_name) in COLOUR_READINGS_MAP:
+        distance = math.dist(color_data, reference_point)
+        if distance < min_distance:
+            min_distance = distance
+            best_match = color_name
+    
+    print("Shortest Distance: " + min_distance)
+    return best_match
+        
+# continual loop -> collect 100 readings, then try to classify colour 
+# maybe add some delay
 
 wait_ready_sensors(True)
 print("Finished initialization. Sensor is ready.")
 
-# run for each of the colours
-for color in ["red", "green", "blue", "yellow", "orange"]:
-    print("Hold the {} block in front of the sensor for 3 seconds. When you're done, press Enter...".format(color))
-    sleep(3) 
-    print("Starting data collection...")
-
-    # get the color data
+try:
     while True:
-        # detect if enter is pressed then exit
-        if keyboard.is_pressed('enter'):
-            print("Data collection stopped.")
-            break
-
-        # collect colour until enter is pressed
         color_data = COLOUR_SENSOR.get_value()
-
-        total_red = []
-        total_green = []
-        total_blue = []
-        total_intensity = []
-
-        if color_data is not None:
-            r, g, b = color_data[:3]
-
-            # normalization
-            denominator = float(r) + float(g) + float(b)
-            if denominator != 0:
-                r_norm = float(r) / denominator
-                g_norm = float(g) / denominator
-                b_norm = float(b) / denominator
-            else:
-                r_norm = g_norm = b_norm = 0
-
-            total_red.append(r_norm)
-            total_green.append(g_norm)
-            total_blue.append(b_norm)
-            total_intensity.append(denominator)
-
-        # calculate the average of the collected data
-        avg_red = sum(total_red) / len(total_red) if total_red else 0
-        avg_green = sum(total_green) / len(total_green) if total_green else 0
-        avg_blue = sum(total_blue) / len(total_blue) if total_blue else 0
-        avg_intensity = sum(total_intensity) / len(total_intensity) if total_intensity else 0
-
-        COLOUR_MAP.update({color: (avg_red, avg_green, avg_blue, avg_intensity)})
-
-# write the data to a csv file
-with open(COLOUR_SENSOR_DATA_FILE, "w") as f:
-    f.write("colour,red,green,blue,intensity\n")
-    for color, values in COLOUR_MAP.items():
-        f.write("{},{},{},{},{}\n".format(color, *values))
+        if color_data is not None: 
+            normalized_color_data = normalize_reading(color_data)
+            classified_color = classify_color(normalized_color_data)
+            print("COLOUR: " + classified_color)
+        
+        sleep(0.25) # polling rate is every 0.25 sec
+    
+except KeyboardInterrupt:
+    print("Finished program")
